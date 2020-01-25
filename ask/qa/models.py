110fd321527ba5ec88979838f148f68b8d1a1736
TheEgid <egid.f1@gmail.com>
2020-01-25 15:25:08 +0300

    Update django version

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,16 +1,27 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
-class Post(object):
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question (models.Model):
     title = models.CharField(max_length=255)
-    content = models.TextField()
-    creation_date = models.DateTimeField(blank=True)
-    def __unicode__(self):
-        return self.title
-    def get_absolute_url(self):
-        return '/question/%d/' % self.pk
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+    objects = QuestionManager()
 
-    class Meta:
-        db_table = 'qa__question'
-        ordering = ['-creation_date']
+
+class Answer (models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.OneToOneField(Question,on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ sudo apt install python3.5-dev
 sudo unlink /usr/bin/python3
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 sudo python3 -m pip install gunicorn
-sudo python3 -m pip install django==2.0
+sudo python3 -m pip install django==2.2.2
 sudo python3 -m pip install mysqlclient
 
 
